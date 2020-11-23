c3e2fa480a8fee53432f5343bc2bc25c2d56679b
Eri Nagashima <es6.ukf@gmail.com>
2020-11-23 18:36:35 +0900

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Polyglot from 'node-polyglot';
 
+let currentLocale = 'ja';
+
 class TranslationApp {
   constructor() {
     this.polyglot = new Polyglot();
@@ -10,6 +12,13 @@ class TranslationApp {
       現在のLocaleに合わせて、polyglotにメッセージをセットします。
       メッセージのセットにはpolyglot.extend()を利用します。
     */
+    if (currentLocale === 'ja') {
+      this.polyglot.extend({
+        hello: 'こんにちは、世界',
+      });
+
+      this.polyglot.t('hello');
+    }
   }
 
   updateLocale(e) {
@@ -22,15 +31,22 @@ class TranslationApp {
     /*
       mainというidがセットされた要素の下にh1タグで現在のlocaleに応じて、メッセージを表示します。 
     */
+
+    const mainField = document.getElementById('main');
+    const message = document.createElement('h1');
+    mainField.appendChild(message);
+
+    message.innerHTML = this.setup(currentLocale);
   }
-
 }
 
+const tranlatedMessage = new TranslationApp({});
+tranlatedMessage.showMessage();
 
-{
-  const button1 = document.getElementById('button1');
-  button1.addEventListener("click", app.updateLocale);
-  
-  const button2 = document.getElementById('button2');
-  button2.addEventListener("click", app.updateLocale);
-}
+// {
+// const button1 = document.getElementById('button1');
+// button1.addEventListener('click', app.updateLocale);
+
+// const button2 = document.getElementById('button2');
+// button2.addEventListener("click", app.updateLocale);
+// }
